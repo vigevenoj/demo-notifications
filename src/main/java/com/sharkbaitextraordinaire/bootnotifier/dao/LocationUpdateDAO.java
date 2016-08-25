047c0e7885f49b1f6bd4f5500cbfe82d223fb1e7
Jacob Vigeveno <vigevenoj@gmail.com>
2016-08-24 22:20:29 -0700

    Use desc sort order for location update timestamps. Map rows correctly

--- src/main/java/com/sharkbaitextraordinaire/bootnotifier/dao/LocationUpdateDAO.java
+++ src/main/java/com/sharkbaitextraordinaire/bootnotifier/dao/LocationUpdateDAO.java
@@ -2,6 +2,8 @@ package com.sharkbaitextraordinaire.bootnotifier.dao;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -14,25 +16,26 @@ public class LocationUpdateDAO {
 
 	@Autowired
 	private JdbcTemplate jdbcTemplate;
+	private static final Logger logger = LoggerFactory.getLogger(LocationUpdateDAO.class);
 	
 	public List<LocationUpdate> findAll() {
-		String sql = "select _type, lat, lon, acc, tst, batt, event from locationupdates order by tst asc";
-		return jdbcTemplate.query(sql, new BeanPropertyRowMapper<LocationUpdate>(LocationUpdate.class));
+		String sql = "select _type, lat, lon, acc, tst, batt, event from locationupdates order by tst desc";
+		return jdbcTemplate.query(sql, new LocationUpdateRowMapper());
 	}
 	
 	public LocationUpdate findLatest() {
-		String sql = "select _type, lat, lon, acc, tst, batt from locationupdates order by tst desc limit 1";
-		return jdbcTemplate.queryForObject(sql, LocationUpdate.class);
+		String sql = "select _type, lat, lon, acc, tst, batt, event from locationupdates order by tst desc limit 1";
+		return jdbcTemplate.queryForObject(sql, new LocationUpdateRowMapper());
 	}
 	
 	public void insert(LocationUpdate location) {
 		String sql = "insert into locationupdates "
-				+ "(_type, lat, lon, acc, tst, batt) "
+				+ "(_type, lat, lon, acc, tst, batt, event) "
 				+ "values "
-				+ "(?, ?, ?, ?, ?, ?)";
+				+ "(?, ?, ?, ?, ?, ?, ?)";
 		jdbcTemplate.update(sql, new Object[] {location.get_type(), 
 				location.getLatitude(), location.getLongitude(),
 				location.getAccuracy(), location.getTimestamp(),
-				location.getBattery() });
+				location.getBattery(), location.getEvent() });
 	}
 }
---
+++ src/main/java/com/sharkbaitextraordinaire/bootnotifier/dao/LocationUpdateRowMapper.java
@@ -0,0 +1,25 @@
+package com.sharkbaitextraordinaire.bootnotifier.dao;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.springframework.jdbc.core.RowMapper;
+
+import com.sharkbaitextraordinaire.bootnotifier.model.LocationUpdate;
+
+public class LocationUpdateRowMapper implements RowMapper<LocationUpdate> {
+
+	@Override
+	public LocationUpdate mapRow(ResultSet rs, int rowNum) throws SQLException {
+		LocationUpdate location = new LocationUpdate();
+		location.set_type(rs.getString("_type"));
+		location.setLatitude(rs.getDouble("lat"));
+		location.setLongitude(rs.getDouble("lon"));
+		location.setAccuracy(rs.getString("acc"));
+		location.setBattery(rs.getString("batt"));
+		location.setTimestamp(rs.getLong("tst"));
+		location.setEvent(rs.getString("event"));
+		return location;
+	}
+
+}
