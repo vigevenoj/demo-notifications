fc17063d5780c379817993c7c63444c2106253ab
Jacob Vigeveno <vigevenoj@gmail.com>
2016-08-27 21:51:38 -0700

    Clean up imports

--- src/main/java/com/sharkbaitextraordinaire/bootnotifier/dao/EarthquakeDAO.java
+++ src/main/java/com/sharkbaitextraordinaire/bootnotifier/dao/EarthquakeDAO.java
@@ -6,7 +6,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.EmptyResultDataAccessException;
-import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
