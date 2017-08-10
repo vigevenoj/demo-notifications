8ca510de9ff10f06cf1352fbf41c9e5f5a2fe4bf
Jacob Vigeveno <vigevenoj@gmail.com>
2017-08-10 09:00:44 -0700

    properly deserialize notifications from json

--- notifications/src/main/java/com/sharkbaitextraordinaire/notifications/Notification.java
+++ notifications/src/main/java/com/sharkbaitextraordinaire/notifications/Notification.java
@@ -1,19 +1,27 @@
 package com.sharkbaitextraordinaire.notifications;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonPOJOBuilder;
 
+@JsonDeserialize(builder = Notification.NotificationBuilder.class)
 public class Notification {
 	
 	private String origin;
 	private String title;
 	private String message;
 	
+	@JsonPOJOBuilder
 	public static class NotificationBuilder {
 		private String origin;
 		private String title;
 		private String message;
 		
+		@JsonProperty("origin")
 		public NotificationBuilder origin(String origin) { this.origin = origin; return this; }
+		@JsonProperty("title")
 		public NotificationBuilder title(String title) { this.title = title; return this; }
+		@JsonProperty("message")
 		public NotificationBuilder message(String message) { this.message = message; return this; }
 		
 		public Notification build() {
@@ -26,6 +34,8 @@ public class Notification {
 		this.title = builder.title;
 		this.message = builder.message;
 	}
+	
+	public Notification() {}
 	
 	public String getOrigin() {
 		return origin;
